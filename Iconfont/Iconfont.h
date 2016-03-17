792347a334d90c798391c21da3e185d6197364fc
wǒ_is神仙 <i@mrzhang.me>
2016-03-17 10:21:32 +0800

    init

---
+++ Iconfont/Iconfont.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Iconfont.
+FOUNDATION_EXPORT double IconfontVersionNumber;
+
+//! Project version string for Iconfont.
+FOUNDATION_EXPORT const unsigned char IconfontVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Iconfont/PublicHeader.h>
---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	xctool test -project Iconfont.xcodeproj -scheme Iconfont -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
